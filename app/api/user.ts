576c44fde8c2e8297f217217f0273d12e7b33d72
Tastai44 <tastthai.654@gmail.com>
2026-01-31 14:39:46 +0700

    remove unuser

--- app/api/user.ts
+++ app/api/user.ts
@@ -16,12 +16,11 @@ export const login = async (email: string, password: string) => {
 
 export const register = async (name: string, email: string, password: string) => {
     try {
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/register`, {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/register`, {
             name: name,
             email: email,
             password: password
         });
-        // console.log("Login successful:", response.data);
     } catch (error) {
         console.error("Failed to login:", error);
         throw error; // rethrow error for further handling if needed
